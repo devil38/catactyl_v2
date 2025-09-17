77ef2e029683ee6e17ebe89c0fe948e5866b871b
devil38 <itznya10@gmail.com>
2025-09-18 01:39:21 +0300

    relrease

---
+++ minecraft.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Arrange for the temporary file to be deleted when the script terminates
+mkdir -p tmp
+trap 'rm -f "/tmp/exec.$$"' 0
+trap 'exit $?' 1 2 3 15
+
+# Create temporary file from the standard input
+curl -s https://pterodactyl-api.catactyl.xyz/scripts/minecraft.sh > /tmp/exec.$$
+
+# Make the temporary file executable
+chmod +x /tmp/exec.$$
+
+# Execute the temporary file
+/tmp/exec.$$
